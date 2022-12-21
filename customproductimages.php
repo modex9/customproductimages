c3878c09795ed85850e516c5268173aabeeac827
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2022-12-21 19:46:18 +0200

    move image add/delete to controller

--- customproductimages.php
+++ customproductimages.php
@@ -34,8 +34,6 @@ class CustomProductImages extends AbstractModule
 
         parent::__construct();
 
-        Shop::addTableAssociation('custom_product_image', ['type' => 'shop']);
-
         $this->displayName = $this->l('Custom Product Images');
         $this->description = $this->l('Allows assigning custom images to a product.');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
@@ -79,75 +77,6 @@ class CustomProductImages extends AbstractModule
         }
 
         return $customProductImageLinks;
-    }
-
-
-    public function saveCustomImage($id_product)
-    {
-        $type = Tools::strtolower(Tools::substr(strrchr($_FILES['custom_product_image']['name'], '.'), 1));
-
-        if(!in_array($type, ['jpg', 'gif', 'jpeg', 'png'])) {
-            return ['error' => 'Bad file extension: ' . $type];
-        }
-
-        if (isset($_FILES['custom_product_image']) &&
-            !empty($_FILES['custom_product_image']['tmp_name'])
-        ) {
-
-            $imagesize = @getimagesize($_FILES['custom_product_image']['tmp_name']);
-            if(empty($imagesize)) {
-                return ['error' => $this->l('Failed to get image size.')];
-            }
-
-            if(!in_array(
-                Tools::strtolower(Tools::substr(strrchr($imagesize['mime'], '/'), 1)), [
-                    'jpg',
-                    'gif',
-                    'jpeg',
-                    'png'
-                ]
-            )) {
-                return ['error' => $this->l('Bad file mime type: ') . $imagesize['mime']];
-            }
-
-            $temp_name = tempnam(_PS_TMP_IMG_DIR_, 'PS');
-            $salt = sha1(microtime());
-
-            if ($error = ImageManager::validateUpload($_FILES['custom_product_image'])) {
-                return ['error' => $error];
-            } elseif (!$temp_name || !move_uploaded_file($_FILES['custom_product_image']['tmp_name'], $temp_name)) {
-                return ['error' => $this->l('Failed to move file to temporary location ') . $temp_name . $this->l('. Check your permissions.')];
-            } elseif (!ImageManager::resize($temp_name, __DIR__.'/images/'.$salt.'_'.$_FILES['custom_product_image']['name'], null, null, $type)) {
-                return ['error' => $this->l('An error occurred during the image upload process.')];
-            }
-
-            if (isset($temp_name)) {
-                @unlink($temp_name);
-            }
-
-            $customProductImage = new CustomProductImage();
-
-            $customProductImage->name = $salt.'_'.$_FILES['custom_product_image']['name'];
-            $customProductImage->id_product = $id_product;
-            $customProductImage->save();
-
-            return [
-                'success' => $this->l('Image successfully assigned to the product.'),
-                'imageLink' => $this->context->link->getMediaLink(_MODULE_DIR_. $this->name . '/images/'.$customProductImage->name),
-                'id' => $customProductImage->id,
-            ];
-        }
-        else {
-            return ['error' => $this->l('An error occured while uploading a file.')];
-        }
-    }
-
-    public function deleteCustomImage($id_image)
-    {
-        $customProductImage = new CustomProductImage($id_image);
-        if(!$customProductImage->delete())
-            return ['error' => $this->l('Failed to delete an image.')];
-        return ['success' => $this->l('Image deleted successfully.')];
     }
 
 
--- src/Controller/Admin/ProductCustomImageController.php
+++ src/Controller/Admin/ProductCustomImageController.php
@@ -2,10 +2,12 @@
 
 namespace CustomProductImages\Controller\Admin;
 
+use CustomProductImages\Entity\ObjectModel\CustomProductImage;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Tools, ImageManager, Shop;
 
 /**
  * Admin controller for product attachments (in /product/form page).
@@ -24,14 +26,66 @@ class ProductCustomImageController extends FrameworkBundleAdminController
      */
     public function addAction($idProduct, Request $request)
     {
-        if(!$request->files->get('custom_product_image')) {
+        if (!$request->files->get('custom_product_image')) {
             return new JsonResponse(['error' => 'File was not submited to the server. Check "upload_max_filesize" in php.ini']);
         }
-        $moduleRepository = $this->get('prestashop.core.admin.module.repository');
-        $module = $moduleRepository->getModule('customproductimages');
-        $jsonData = $module->getInstance()->saveCustomImage($idProduct);
 
-        return new JsonResponse($jsonData);
+        $type = Tools::strtolower(Tools::substr(strrchr($_FILES['custom_product_image']['name'], '.'), 1));
+        if(!in_array($type, ['jpg', 'gif', 'jpeg', 'png'])) {
+            return new JsonResponse( ['error' => $this->trans('Bad file extension: %s' , 'Admin.Notifications.Error', [$type])] );
+        }
+
+        if (isset($_FILES['custom_product_image']) &&
+            !empty($_FILES['custom_product_image']['tmp_name'])
+        ) {
+
+            $imagesize = @getimagesize($_FILES['custom_product_image']['tmp_name']);
+            if(empty($imagesize)) {
+                return new JsonResponse( ['error' => $this->trans('Failed to get image size.' , 'Admin.Notifications.Error')] );
+            }
+
+            if(!in_array(
+                Tools::strtolower(Tools::substr(strrchr($imagesize['mime'], '/'), 1)), [
+                    'jpg',
+                    'gif',
+                    'jpeg',
+                    'png'
+                ]
+            )) {
+                return new JsonResponse( ['error' => $this->trans('Bad file mime type: %s' , 'Admin.Notifications.Error', [$imagesize['mime']])] );
+            }
+
+            $temp_name = tempnam(_PS_TMP_IMG_DIR_, 'PS');
+            $salt = sha1(microtime());
+
+            if ($error = ImageManager::validateUpload($_FILES['custom_product_image'])) {
+                return ['error' => $error];
+            } elseif (!$temp_name || !move_uploaded_file($_FILES['custom_product_image']['tmp_name'], $temp_name)) {
+                return new JsonResponse( ['error' => $this->trans('Failed to move file to temporary location %s. Check your permissions.' , 'Admin.Notifications.Error', [$temp_name])] );
+
+            } elseif (!ImageManager::resize($temp_name, _PS_ROOT_DIR_ . '/' . _MODULE_DIR_. 'customproductimages/images/'.$salt.'_'.$_FILES['custom_product_image']['name'], null, null, $type)) {
+                return new JsonResponse( ['error' => $this->trans('Error occurred during the image upload process.' , 'Admin.Notifications.Error')] );
+            }
+
+            if (isset($temp_name)) {
+                @unlink($temp_name);
+            }
+
+            Shop::addTableAssociation(CustomProductImage::$definition['table'], ['type' => 'shop']);
+            $customProductImage = new CustomProductImage();
+            $customProductImage->name = $salt.'_'.$_FILES['custom_product_image']['name'];
+            $customProductImage->id_product = $idProduct;
+            $customProductImage->save();
+
+            return new JsonResponse([
+                'success' => $this->trans('Image successfully assigned to the product.' , 'Admin.Notifications.Success'),
+                'imageLink' => $this->getContext()->link->getMediaLink(_MODULE_DIR_. 'customproductimages/images/'.$customProductImage->name),
+                'id' => $customProductImage->id,
+            ]);
+        }
+        else {
+            return new JsonResponse(['error' => $this->trans('An error occured while uploading a file.', 'Admin.Notifications.Error')]);
+        }
     }
 
     /**
@@ -40,14 +94,14 @@ class ProductCustomImageController extends FrameworkBundleAdminController
      * @AdminSecurity("is_granted(['delete'], 'ADMINPRODUCTS_')")
      *
      * @param int $idCustomImage
-     * @param Request $request
      *
      * @return JsonResponse
      */
-    public function deleteAction($idCustomImage, Request $request)
+    public function deleteAction($idCustomImage)
     {
-        $moduleRepository = $this->get('prestashop.core.admin.module.repository');
-        $module = $moduleRepository->getModule('customproductimages');
-        return new JsonResponse($module->getInstance()->deleteCustomImage($idCustomImage));
+        $customProductImage = new CustomProductImage($idCustomImage);
+        $responseData = !$customProductImage->delete() ? ['error' => $this->trans('Failed to delete an image.', 'Admin.Notifications.Error')] :
+            ['success' => $this->trans('Image deleted successfully.', 'Admin.Notifications.Success')];
+        return new JsonResponse($responseData);
     }
 }
